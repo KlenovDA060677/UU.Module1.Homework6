87d7116bda72150187bf84ea6cabd339f5efb967
K_Dmitry <kldma@rambler.ru>
2024-06-20 02:03:38 +0500

    Add files via upload

---
+++ Homework6.py
@@ -0,0 +1,37 @@
+# Практическое задание по теме: "Словари и множества"
+
+# Создаем словарь и выводим данные словаря на экран
+my_dict = {"anton": 1980, "aleksandr": 1983, "andrey": 1987, "boris": 1988,
+           "konstantin": 1992, "peter": 1988, "mikle": 1993}
+print(my_dict)
+
+# Выводим значение по заданному ключу
+name = "peter"
+print(f"Год рождения {name.title()} - {my_dict[name]}")
+
+# Пытаемся вывести значение из словаря по несуществующему ключу
+name = "dmitry"
+print(my_dict.get(name, f"Данных о '{name.title()}' нет в словаре!"))
+
+# Добавляем несколько пар в словарь
+new_users = {"dmitry": 1977, "ivan": 1979}
+my_dict.update(new_users)
+print(f"В словарь были добавлены новые данные \n {list(new_users.items())}")
+
+# Удаляем пару из словаря
+block_user = "aleksandr"
+print (f"Из словаря удалили пару- '{block_user}: {my_dict.pop(block_user)}'")
+print(my_dict)
+
+# Создаем множество
+my_set = {1,1,1,"Дмитрий","Антон","дмитрий",3,3,5,5,7,7,7,"Дмитрий","Антон",True, True}
+print(my_set)
+
+# Добавляем несколько значений во множество
+my_set.add(6)
+my_set.add(6)
+print(my_set)
+
+# Удаляем значение из множества
+my_set.discard("дмитрий")
+print(my_set)
